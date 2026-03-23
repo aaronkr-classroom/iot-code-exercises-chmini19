990231e33f3e97687338064cf1f7a3df934035f2
chmini19 <kjw20061222@gmail.com>
2026-03-23 11:40:18 +0900

    Add files via upload

---
+++ code/3_color.py
@@ -0,0 +1,14 @@
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+my_color = (24, 75, 189)
+my_color = (24, 75, 0)
+
+my_color = [
+    black,white,red,
+    green,blue,my_color
+    ]
+for color in my_color:
+    print(color)
